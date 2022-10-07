68be5e5c01f22aa4a6178383a05eb2b15e59886d
Egor <egor@opensrc.club>
2022-10-07 18:00:08 +0300

    maxwell.lisp: use less built-in stuff

--- maxwell.lisp
+++ maxwell.lisp
@@ -15,38 +15,44 @@ DEFINE((
 ; these work in cond, but they need to be added to the environment:
 ; NIL T
 ; stuff we don't define:
-; CADAR CADDR CADR CDAR CAAR NULL DEFINE
+; DEFINE
+(QNULL (LAMBDA (X) (EQ X NIL) ))
+(QCAAR (LAMBDA (P) (CAR (CAR P)) ))
+(QCADR (LAMBDA (P) (CAR (CDR P)) ))
+(QCDAR (LAMBDA (P) (CDR (CAR P)) ))
+(QCADAR (LAMBDA (P) (CAR (CDR (CAR P))) ))
+(QCADDR (LAMBDA (P) (CAR (CDR (CDR P))) ))
 (QEQUAL (LAMBDA (X Y) (COND ((ATOM X) (COND ((ATOM Y) (EQ X Y)) (T NIL)))
                             ((QEQUAL (CAR X) (CAR Y)) (QEQUAL (CDR X) (CDR Y)))
                             (T NIL)) ))
-(QPAIRLIS (LAMBDA (X Y A) (COND ((NULL X) A) (T (CONS (CONS (CAR X) (CAR Y))
-                                                (QPAIRLIS (CDR X) (CDR Y) A)))) ))
-(QASSOC (LAMBDA (X A) (COND ((QEQUAL (CAAR A) X) (CAR A)) (T (QASSOC X (CDR A)))) ))
+(QPAIRLIS (LAMBDA (X Y A) (COND ((QNULL X) A) (T (CONS (CONS (CAR X) (CAR Y))
+                                                       (QPAIRLIS (CDR X) (CDR Y) A)))) ))
+(QASSOC (LAMBDA (X A) (COND ((QEQUAL (QCAAR A) X) (CAR A)) (T (QASSOC X (CDR A)))) ))
 (QEVALQUOTE (LAMBDA (FN X) (QAPPLY FN X NIL) ))
 
 (QAPPLY (LAMBDA (FN X A)
-        (COND ((ATOM FN) (COND ((EQ FN (QUOTE CAR)) (CAAR X))
-                               ((EQ FN (QUOTE CDR)) (CDAR X))
-                               ((EQ FN (QUOTE CONS)) (CONS (CAR X) (CADR X)))
+        (COND ((ATOM FN) (COND ((EQ FN (QUOTE CAR)) (QCAAR X))
+                               ((EQ FN (QUOTE CDR)) (QCDAR X))
+                               ((EQ FN (QUOTE CONS)) (CONS (CAR X) (QCADR X)))
                                ((EQ FN (QUOTE ATOM)) (ATOM (CAR X)))
-                               ((EQ FN (QUOTE EQ)) (EQ (CAR X) (CADR X)))
+                               ((EQ FN (QUOTE EQ)) (EQ (CAR X) (QCADR X)))
                                (T (QAPPLY (QEVAL FN A) X A))))
-              ((EQ (CAR FN) (QUOTE LAMBDA)) (QEVAL (CADDR FN) (QPAIRLIS (CADR FN) X A)))
-              ((EQ (CAR FN) (QUOTE LABEL)) (QAPPLY (CADDR FN) X (CONS (CONS (CADR FN)
-                                                                            (CADDR FN)) A)))) ))
+              ((EQ (CAR FN) (QUOTE LAMBDA)) (QEVAL (QCADDR FN) (QPAIRLIS (QCADR FN) X A)))
+              ((EQ (CAR FN) (QUOTE LABEL)) (QAPPLY (QCADDR FN) X (CONS (CONS (QCADR FN)
+                                                                             (QCADDR FN)) A)))) ))
 
 (QEVAL (LAMBDA (E A)
        (COND ((ATOM E) (CDR (QASSOC E A)))
              ((ATOM (CAR E)) (COND
-                               ((EQ (CAR E) (QUOTE QUOTE)) (CADR E))
+                               ((EQ (CAR E) (QUOTE QUOTE)) (QCADR E))
                                ((EQ (CAR E) (QUOTE COND)) (QEVCON (CDR E) A))
                                (T (QAPPLY (CAR E) (QEVLIS (CDR E) A) A))))
              (T (QAPPLY (CAR E) (QEVLIS (CDR E) A) A))) ))
 
-(QEVCON (LAMBDA (C A) (COND ((QEVAL (CAAR C) A) (QEVAL (CADAR C) A))
+(QEVCON (LAMBDA (C A) (COND ((QEVAL (QCAAR C) A) (QEVAL (QCADAR C) A))
                             (T (QEVCON (CDR C) A))) ))
 
-(QEVLIS (LAMBDA (M A) (COND ((NULL M) NIL)
+(QEVLIS (LAMBDA (M A) (COND ((QNULL M) NIL)
                             (T (CONS (QEVAL (CAR M) A) (QEVLIS (CDR M) A)))) ))
 
 
